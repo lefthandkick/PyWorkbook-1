cc7b6cf3e49b220ecc9f36c1f1ba77749676d6b4
zaatas <zaatas@me.com>
2019-05-08 21:41:57 -0600

    Done with this example

--- population.py
+++ population.py
@@ -1,18 +1,35 @@
-def population_growth(initial_pop, annual_growth_rate, target_pop):
+def population_growth(initial_pop, annual_growth_rate, target_pop) -> int:
     if annual_growth_rate <= 0:
-        return "invalid growth rate"
+        print('invalid growth rate - start over')
+        return -1
 
-    count: int = 0
-    new_pop: int = 0
+    count:   int = 0
+    new_pop: float = 0
+
+    def exponent(value: float, times: int) -> float:
+        ret_val: float = 1
+        for i in range(times):
+            ret_val = ret_val * value
+
+        return ret_val
 
     while new_pop < target_pop:
         count += 1
-        new_pop = initial_pop * (1 + annual_growth_rate) ** count
-        print(new_pop)
+
+        new_pop = exponent(float((1 + annual_growth_rate)), count)
+        new_pop = initial_pop * new_pop
 
     return count
 
 
-if __name__ == "__main__":
-    print('test')
-    print(population_growth(5000, 0.1, 10000))
+def main():
+    pop_today:   float = 5000.0
+    growth_rate: float = 0.1
+    target_pop:  float = 10000.0
+
+    print(f'\n It will take {population_growth(pop_today, growth_rate, target_pop)} years.')
+    print(f'\n to get to a population of {int(target_pop)} ')
+
+
+main()
+
